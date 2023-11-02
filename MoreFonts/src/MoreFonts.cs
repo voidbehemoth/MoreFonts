3891f5b4c8a28c30ee270e1b7421372853958145
VoidBehemoth <VoidBehemoth@gmail.com>
2023-11-01 18:02:53 -0600

    Add project files.

---
+++ MoreFonts/src/MoreFonts.cs
@@ -0,0 +1,23 @@
+﻿using SML;
+
+namespace MoreFonts
+{
+    [Mod.SalemMod]
+    public class Main
+    {
+        public static void Start()
+        {
+            MoreFonts.Utils.Logger.Log(ModInfo.PLUGIN_NAME + " has loaded!");
+        }
+    }
+
+
+    public static class ModInfo
+    {
+        public const string PLUGIN_GUID = "MoreFonts";
+
+        public const string PLUGIN_NAME = "MoreFonts";
+
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
